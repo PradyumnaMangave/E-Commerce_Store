a3b3a354f5e305a9db91659183673029d0c13666
PradyumnaMangave <mangavepradyumna.gmail.com>
2023-09-12 14:41:00 +0530

    created structure for register page

---
+++ resources/views/Pages/register.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.master')
+@section('name','Register Page')
+@section('content')
+    <section class="login-page">
+        <div class="LogIn-form-box">
+            <div class="login-title">Register</div>
+            <div class="login-form">
+                <form action="" method="post">
+                    <div class="field">
+                        <label for="name">Name</label>
+                        <input type="text" id="name" name="name" placeholder="Enter Your Name">
+                    </div>
+
+                    <div class="field">
+                        <label for="email">Email</label>
+                        <input type="email" id="email" name="email" placeholder="Enter Your Mail ID">
+                    </div>
+
+                    <div class="field">
+                        <label for="password">Password</label>
+                        <input type="password" id="password" name="password" placeholder="Enter Your Password">
+                    </div>
+
+                    <div class="field">
+                        <label for="password_confirmation">Confirm Password</label>
+                        <input type="password" id="password" name="password_confirmation" placeholder="Confirm Your Password">
+                    </div>
+
+                    <div class="field">
+                        <button type="submit" class="btn btn-primary btn-block">Register</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </section>
+@endsection
