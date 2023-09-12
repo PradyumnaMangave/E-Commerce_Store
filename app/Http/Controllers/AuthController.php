ed71c611b132cb6b4edd11886b5218d8443eb94f
PradyumnaMangave <mangavepradyumna.gmail.com>
2023-09-12 14:40:06 +0530

    Created auth controller files for the pages

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    //Show Login page
+    public function showLogin()
+    {
+        return view('pages.login');
+    }
+
+
+    //show register page
+    public function showRegister()
+    {
+        return view('pages.register');
+    }
+
+
+    //register user
+
+    //login user
+
+    //reset password
+}
