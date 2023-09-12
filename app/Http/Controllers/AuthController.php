50874882e432612d95a8a53fa910125865d99b4a
PradyumnaMangave <mangavepradyumna.gmail.com>
2023-09-12 23:17:42 +0530

    added authentication

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -21,6 +24,27 @@ class AuthController extends Controller
 
 
     //register user
+    public function postRegister(Request $request){
+        
+        //validation
+        $request->validate([
+            'name' => 'required|min:3|max:255',
+            'email' => 'required|email|max:255|unique:users',
+            'password' => 'required|min:8|confirmed'
+        ]);
+
+        //registeration
+        $user = User::create([
+            'name' =>$request->name,
+            'email' =>$request->email,
+            'password' =>Hash::make($request->password),
+        ]);
+
+        //login
+            Auth::login($user);
+
+            return back()->with('success','Successfully Logged In');
+    }
 
     //login user
 
