8eb3407455514c24c657e7937beec6b8fede907c
PradyumnaMangave <mangavepradyumna.gmail.com>
2023-09-13 11:03:02 +0530

    restructured login page with required details

---
+++ resources/views/Pages/login.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.master')
+@section('name','LogIn Page')
+@section('content')
+<section class="login-page">
+        <div class="login-form-box">
+            <div class="login-title">Login</div>
+            <div class="login-form">
+                <form action="{{route('login')}}" method="post">
+                    @csrf
+
+                    <div class="field">
+                        <label for="email">Email</label>
+                        <input type="email" id="email" name="email" class=" @error('email') has-error @enderror" placeholder="Enter Your Mail ID">
+                        @error('email')
+                        <span class="field-error">{{$message}}</span>
+                        @enderror
+                    </div>
+
+                    <div class="field">
+                        <label for="password">Password</label>
+                        <input type="password" id="password" name="password" class=" @error('password') has-error @enderror" placeholder="Enter Your Password">
+                        @error('password')
+                        <span class="field-error">{{$message}}</span>
+                        @enderror
+                    </div>
+
+                    <div class="field">
+                        <button type="submit" class="btn btn-primary btn-block">Login</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </section>
+@endsection
