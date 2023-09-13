db8f77e2ad11b5276095b9015dbd871decf1e159
PradyumnaMangave <mangavepradyumna.gmail.com>
2023-09-13 18:08:58 +0530

    Created navigation bar structure for dashboard

--- resources/views/admin/Partials/nav.blade.php
+++ resources/views/admin/Partials/nav.blade.php
@@ -1,1 +1,29 @@
-<h1>Nav</h1>
+<aside class="side-nav">
+
+<div class="log">
+<img src="{{asset('images/logo.svg')}}" alt="">
+Admin Panel
+</div>
+
+<ul>
+    <li>
+        <a href="">Dashboard</a>
+    </li>
+
+    <li>
+        <a href="">Products</a>
+    </li>
+
+    <li>
+        <a href="">Categories</a>
+    </li>
+
+    <li>
+        <a href="">Orders</a>
+    </li>
+
+    
+
+</ul>
+
+</aside>
