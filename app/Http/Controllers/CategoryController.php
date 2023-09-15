b156c970edb3bae591da9b4f90fca55901b6ab8e
PradyumnaMangave <mangavepradyumna.gmail.com>
2023-09-15 17:37:14 +0530

    Created function for storing categories

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
@@ -9,5 +10,21 @@ class CategoryController extends Controller
     //index
     public function index(){
         return view ('admin.Pages.Categories.index');
+    }
+
+    public function store(Request $request){
+        
+        //validate
+        $request->validate([
+            'name' => 'required|unique:categories|max:255'
+        ]);
+
+        //store
+        $category = new Category();
+        $category->name = $request->name;
+        $category->save();
+
+        //return view
+        return back()->with('success','Category Saved');
     }
 }
