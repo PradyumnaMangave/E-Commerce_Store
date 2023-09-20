c53012097bf81a10dacb1461855ca9fc18abf8a8
PradyumnaMangave <mangavepradyumna.gmail.com>
2023-09-20 13:01:25 +0530

    linked product box blade file with home

--- resources/views/Pages/home.blade.php
+++ resources/views/Pages/home.blade.php
@@ -6,6 +6,16 @@
 
         @include('pages.components.home.header')
 
+        <section class="products-section">
+            <div class="container">
+                <h1 class="section-title">Featured Products</h1>
+                <div class="products-row">
+                    @foreach($products as $product)
+                        <x-product-box :product="$product" />
+                    @endforeach
+                </div>
+            </div>
+        </section>
     </main>
 
 @endsection
