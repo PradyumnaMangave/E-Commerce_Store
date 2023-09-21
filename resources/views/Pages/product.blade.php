db6e57a462a91510568745deff160a5f1a64c65c
PradyumnaMangave <mangavepradyumna.gmail.com>
2023-09-21 22:24:31 +0530

    Added pop-up message after adding product to cart

--- resources/views/Pages/product.blade.php
+++ resources/views/Pages/product.blade.php
@@ -1,6 +1,21 @@
 @extends('layouts.master')
 @section('name',$product->title)
 @section('content')
+
+    @if(session()->has('addedToCart'))
+        <section class="pop-up">
+            <div class="pop-up-box">
+                <div class="pop-up-title">
+                    {{session()->get('addedToCart')}}
+                </div>
+                <div class="pop-up-actions">
+                    <a href="{{route('home')}}" class="btn btn-outlined">Continue Shopping</a>
+                    <a href="{{route('cart')}}" class="btn btn-primary">Go To Cart</a>
+                </div>
+            </div>
+        </section>
+    @endif
+
     <div class="product-page">
         <div class="container">
             <div class="product-page-row">
