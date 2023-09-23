bc6b77f9f387e22c2d900c8a2044483d7e3e50b8
PradyumnaMangave <mangavepradyumna.gmail.com>
2023-09-23 17:44:03 +0530

    added new yield for head section

--- resources/views/Layouts/master.blade.php
+++ resources/views/Layouts/master.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('name')</title>
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    @yield('head')
 </head>
 <body>
     @include('layouts.partials.nav')
