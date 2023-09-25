2ec880aef2ed767fb177c2954f780fc563c1d451
PradyumnaMangave <mangavepradyumna.gmail.com>
2023-09-25 17:04:29 +0530

    added validation

--- app/Http/Controllers/checkoutController.php
+++ app/Http/Controllers/checkoutController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Stripe\Stripe;
 use App\Models\Cart;
+use App\Models\Order;
 use Stripe\StripeClient;
 use Stripe\PaymentIntent;
 use Illuminate\Http\Request;
@@ -12,7 +13,16 @@ use Stripe\Exception\ApiErrorException;
 class checkoutController extends Controller
 {
     
-    public function stripeCheckout(Request $request){
+    public function stripeCheckout(Request $request)
+    {
+
+        $request->validate([
+            'payment_method_id' => 'required',
+            'name' => 'required|max:255',
+            'email' => 'required|email|max:255',
+            'address' => 'required|max:255',
+        ]);
+        
         Stripe::setApiKey("sk_test_51Nt6L1SE5NmV3W6ahCHcMTl0FCr11hRXxAyYJB3MEMfgmh4bihtTeF3mdFFnLljcGhw6FVwYPIGQYdGLrHXKpALE00TPNmRTle");
         
         try {
@@ -38,8 +48,34 @@ class checkoutController extends Controller
         }
         
 
+        //store order
+        $order = Order::create([
+            'user_id' => auth()->id(),
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'address' => $request->address,
+            'country' => $request->country,
+            'state' => $request->state,
+            'city' => $request->city,
+            'zip' => $request->zip,
+            'stripe_id' => $request->payment_intent_id,
+            'status' => 'pending',
+            'total' => Cart::totalAmount(),
+        ]);
+
+        foreach(session()->get('cart') as $item)
+        {
+            $order->items()->create([
+                'product_id' => $item['product']['id'],
+                'color_id' => $item['color']['id'],
+                'quantity' => $item['quantity'],
+            ]);
+        }
+
+        session()->forget('cart');
         
-        return redirect()-> route('success');
+        return route('success',['order'=> $order]);
     }
     
 }
